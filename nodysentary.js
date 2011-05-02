90bb0beb8e88f82ba5c5094659a091d7684da9a5
Rick Waldron <waldron.rick@gmail.com>
2011-05-02 09:15:33 -0700

    wtf white space? part deux

--- nodysentary.js
+++ nodysentary.js
@@ -29,11 +29,11 @@ function makeRequest() {
 						
             // Terminal dysentary
             console.log(
-							"\n" + 
-							tweet.from_user + " says: " +
-							tweet.text + 
-							"\n (" + tweet.created_at + ")"
-						);
+              "\n" + 
+              tweet.from_user + " says: " +
+              tweet.text + 
+              "\n (" + tweet.created_at + ")"
+            );
 						
             // Don't stutter
             lastId = tweet.id;
@@ -46,10 +46,10 @@ function makeRequest() {
     });
   });
 
-	// write out to request body
-	req.write("data\n");
-	req.write("data\n");
-	req.end();	
-};
+  // write out to request body
+  req.write("data\n");
+  req.write("data\n");
+  req.end();	
+}
 
 setInterval( makeRequest, 1000 );
