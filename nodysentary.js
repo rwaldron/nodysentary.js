7e3b24c33f6a640a93f9f3f47d13c704413ddf0c
Rick Waldron <waldron.rick@gmail.com>
2011-05-02 10:07:50 -0700

    restore o.g.

--- nodysentary.js
+++ nodysentary.js
@@ -5,12 +5,51 @@
  * MIT Licensed
  */
 
-var story = "You have died of ", 
-	fates = [ "measles", "snakebite", "dysentery", "typhoid", "cholera", "exhaustion" ];
+var http = require( "http" ), 
+	options = {
+		host: "search.twitter.com",
+		port: 80,
+		path: "/search.json?q=poopin&rpp=1",
+		method: "GET"
+	}, 
+	lastId = 0;
 
+function makeRequest() {
 
-console.log(
+  var req = http.request(options, function(res) {
 
-	story + fates[ Math.floor( Math.random() * fates.length - 1 ) ]
+    res.on("data", function ( chunk ) {
 
-);
+      // If we get a garbled chunk of data, then fuck it, wait til next time
+      try {
+        var data = JSON.parse( chunk );
+
+        data.results.forEach(function( tweet ) {
+          if ( tweet.id > lastId ) {
+						
+            // Terminal dysentary
+            console.log(
+              "\n" + 
+              tweet.from_user + " says: " +
+              tweet.text + 
+              "\n (" + tweet.created_at + ")"
+            );
+						
+            // Don't stutter
+            lastId = tweet.id;
+          }
+        });
+
+      } catch(e) {
+        //whatevs for now...
+      }
+    });
+  });
+
+  // write out to request body
+  req.write("data\n");
+  req.write("data\n");
+  req.end();	
+}
+
+setInterval( makeRequest, 1000 );
